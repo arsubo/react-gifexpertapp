15e425982fe7364e6d66ba72728c241753e02ab9
Arnoldo Suarez Bonilla <arsubo@gmail.com>
2020-10-10 13:58:42 -0600

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One punch']);
+
+    // const handleAdd = () => {
+    //     // setCategories(['HunterxHunter', ...categories]);
+    //     setCategories(cats => [...cats, 'HunterXHunter'])
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+
+
+            <ol>
+                {
+                    categories.map(category =>
+                        <GifGrid
+                            key={category}
+                            category={category}
+                        />
+                    )
+                }
+            </ol>
+        </>
+    )
+}
+
+export default GifExpertApp;
