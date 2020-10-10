3e97c82373e2a2143a6f1bc712c431e5d94c4a56
Arnoldo Suarez Bonilla <arsubo@gmail.com>
2020-10-10 14:01:56 -0600

    docs agregados

---
+++ docs/precache-manifest.88baef2eb7e51c54a27a815dbed2db2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9015ac29854b72bf8579d40d536a6e64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3fd10e6f4cbafe40141",
+    "url": "/static/css/main.f47b6aa0.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3fd10e6f4cbafe40141",
+    "url": "/static/js/main.4719168c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
